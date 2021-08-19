ce9bf3f92433fbf84372bc38c5589fd10e494958
Juan Manuel Carrizalez Martinez <jcarrizalez@nb-jcarrizalez.qubit.tv>
2021-08-18 22:42:06 -0300

    agrego cache a este servicio

--- app/Services/BookShowService.php
+++ app/Services/BookShowService.php
@@ -3,6 +3,7 @@ declare( strict_types = 1 );
 namespace App\Services;
 
 use App\Models\BookModel;
+use App\Shared\Cache;
 use Exception;
 
 /**
@@ -13,10 +14,15 @@ use Exception;
 class BookShowService
 {
     protected $model;
+    protected $cache;
 
-    public function __construct(BookModel $model)
+    public function __construct(
+        BookModel $model, 
+        Cache $cache
+    )
     {
         $this->model = $model;
+        $this->cache = $cache;
     }
 
     /**
@@ -26,11 +32,28 @@ class BookShowService
      */
     public function make(string $slug, int $page) :string
     {
+        $cache = $slug.$page;
+
+        #si existe en Cache se hace el return
+        if(null !== $response = $this->cache->get($cache)){
+
+            return $response;
+        }
         #si no existe en BD muestro un 404
         if(null === $response = $this->model->show($slug, $page)->first()){
 
             throw new Exception('No existe la pagina ',404);
         }
-        return storage_path("books/{$response['folder']}/pages/{$response['image']}");   
+        $file = storage_path("books/{$response['folder']}/pages/{$response['image']}");
+
+        if(!file_exists($file)){
+
+            throw new Exception('No existe el archivo ',404);
+        }
+
+        #dejo en cache
+        $this->cache->put($cache, $file);
+
+        return $file;
     }
 }
